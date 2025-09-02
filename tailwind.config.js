3edade38c80ef6a606951094f3b7cac04095478a
최진호 <chlwlsgh777@gmail.com>
2025-09-02 22:07:16 +0900

    테일윈드 색상변수 지정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        main: '#38E07A', // Main-Color
+        sub: '#29382E', // Sub-Color
+        text: {
+          DEFAULT: '#FFFFFF', // Text-Color
+          sub: '#9EB8A8', // Text-Sub-Color
+        },
+        background: '#121714', // Background-Color
+      },
+    },
   },
   plugins: [],
 }
-
