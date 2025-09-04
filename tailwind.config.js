196e8b61d66fbb9cbcc6135e9e7cb41e8948f732
jiunlee19 <tearofglass@naver.com>
2025-09-04 10:16:26 +0900

    Revert "테일윈드 색상변수 지정"
    This reverts commit 3edade38c80ef6a606951094f3b7cac04095478a.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: [],
   theme: {
-    extend: {
-      colors: {
-        main: '#38E07A', // Main-Color
-        sub: '#29382E', // Sub-Color
-        text: {
-          DEFAULT: '#FFFFFF', // Text-Color
-          sub: '#9EB8A8', // Text-Sub-Color
-        },
-        background: '#121714', // Background-Color
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
+
