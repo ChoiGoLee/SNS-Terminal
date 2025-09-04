6e8dba9aa671b37b3c06046ed225fcab6cab4e3b
최진호 <chlwlsgh777@gmail.com>
2025-09-04 18:09:28 +0900

    feat(theme): add custom 색상

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,23 @@
 export default {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#38E07A',
+          dark: '#28C965',
+        },
+        background: {
+          DEFAULT: '#000000',
+          surface: '#16181C',
+          border: '#2F3336',
+        },
+        text: {
+          primary: '#E7E9EA',
+          secondary: '#71767B',
+        },
+      },
+    },
   },
   plugins: [],
 }
-
