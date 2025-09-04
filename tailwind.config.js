e4b9d623a0cebbc7280e43882f0e956ece23d72f
jiunlee19 <tearofglass@naver.com>
2025-09-05 00:18:00 +0900

    feat(login):로그인 버튼 UI 구현

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#38E07A',
+          dark: '#28C965',
+        },
+        danger: {
+          DEFAULT: '#DC2626',
+          dark: '#B91C1C',
+        },
+        background: {
+          DEFAULT: '#000000',
+          surface: '#16181C',
+          border: '#2F3336',
+        },
+        text: {
+          primary: '#E7E9EA',
+          secondary: '#71767B',
+        },
+      },
+    },
   },
   plugins: [],
 }
-
