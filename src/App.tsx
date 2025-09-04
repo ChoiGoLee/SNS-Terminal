b4dfc8e90a140c51781d3b10889bbd3f3bf35acc
jiunlee19 <tearofglass@naver.com>
2025-09-04 18:06:59 +0900

    setting(css): reset.css 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
+import './assets/css/reset.css'
 import { Route, Routes, BrowserRouter } from 'react-router-dom'
 import Home from './pages/home/index'
-// import Login from './pages/login/index'
+import Login from './pages/login/index'
 // import PostDetail from './pages/post-detail/index'
 // import PostCreate from './pages/post-create/index'
 // import Profile from './pages/profile/index'
@@ -15,14 +16,14 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
-          {/* <Route path="/login" element={<Login />} />
-          <Route path="/post-detail" element={<PostDetail />} />
-          <Route path="/post-create" element={<PostCreate />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/profile-setting" element={<ProfileSetting />} />
-          <Route path="/messages" element={<Messages />} />
-          <Route path="/notification" element={<Notification />} />
-          <Route path="/settings" element={<Settings />} /> */}
+          <Route path="/login" element={<Login />} />
+          {/* <Route path="/post-detail" element={<PostDetail />} /> */}
+          {/* <Route path="/post-create" element={<PostCreate />} /> */}
+          {/* <Route path="/profile" element={<Profile />} /> */}
+          {/* <Route path="/profile-setting" element={<ProfileSetting />} /> */}
+          {/* <Route path="/messages" element={<Messages />} /> */}
+          {/* <Route path="/notification" element={<Notification />} /> */}
+          {/* <Route path="/settings" element={<Settings />} /> */}
         </Routes>
       </BrowserRouter>
     </div>
