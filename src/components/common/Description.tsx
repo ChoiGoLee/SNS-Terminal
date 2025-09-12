24d25f505dca919df0a60647c9f8336d3a668e80
woori ko <mynameiswooriko@gmail.com>
2025-09-12 11:18:42 +0900

    feat(ui): 설명 컴포넌트에 아이콘 추가 및 설명 속성 정의

--- src/components/common/Description.tsx
+++ src/components/common/Description.tsx
@@ -1,3 +1,33 @@
+import TrashIcon from '../../assets/icons/trash.svg?react'
+import LogoutIcon from '../../assets/icons/logout.svg?react'
+import PostIcon from '../../assets/icons/post.svg?react'
+import HeartIcon from '../../assets/icons/heart-line.svg?react'
+import CommentIcon from '../../assets/icons/comment.svg?react'
+
+interface ExplainProps {
+  iconType: 'resign' | 'logout' | 'post' | 'like' | 'comment' | 'search'
+  title: string
+  description: string
+  buttons?: Array<{
+    text: string
+    onClick: () => void
+    variant: 'primary' | 'secondary' | 'danger'
+  }>
+  showCard?: boolean
+}
+
+//매핑 객체
+const ICON_TYPES = {
+  resign: { icon: TrashIcon, bgColor: 'bg-red-500' },
+  logout: { icon: LogoutIcon, bgColor: 'bg-background-border' },
+  post: { icon: PostIcon, bgColor: 'bg-background-border' },
+  like: { icon: HeartIcon, bgColor: 'bg-background-border' },
+  comment: { icon: CommentIcon, bgColor: 'bg-background-border' },
+  search: 'special', // 예외 처리
+}
+
+const ICON_IMAGES = {}
+
 function Description() {
   return (
     <div>
