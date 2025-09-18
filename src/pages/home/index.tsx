26a3cc5b4f32b96b13de586b1ac6de3de445a1c7
최진호 <chlwlsgh777@gmail.com>
2025-09-18 15:25:30 +0900

    fix/메세지 버블 위치 수정

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,13 +1,6 @@
 import React from 'react'
 import { Header } from '../../components/common/Header'
 import { SideBar } from '../../components/common/SideBar'
-import Markdown from '../../components/common/Markdown'
-import Avatar from '../../components/common/Avatar'
-import CommentButton from '../../components/common/CommentButton'
-import CommentInput from '../../components/common/CommentInput'
-import CommentItem from '../../components/common/CommentItem'
-import LikeButton from '../../components/common/LikeButton'
-import UserLevel from '../../components/common/UserLevel'
 import Description from '../../components/common/Description'
 import {
   MessageBubble,
@@ -22,27 +15,6 @@ function Home(): React.JSX.Element {
       </div>
       <div className="mx-auto border-x border-background-border border-r border-l">
         <Header title="홈" />
-        <div className="w-auto">
-          {/* 메세지가 없을때 */}
-          {sampleMessages.length === 0 ? (
-            <Description
-              iconType="post"
-              title="아직 포스트가 없습니다"
-              description="첫 번째 포스트를 작성해보세요"
-            />
-          ) : (
-            <>
-              {/* 메세지가 있을때 */}
-              {sampleMessages.map((msg) => (
-                <MessageBubble
-                  type={msg.type}
-                  text={msg.text}
-                  userName={msg.userName} // 없으면 기본값 사용
-                />
-              ))}
-            </>
-          )}
-        </div>
       </div>
     </div>
   )
--- src/pages/messages/index.tsx
+++ src/pages/messages/index.tsx
@@ -1,16 +1,43 @@
 import React from 'react'
 import { Header } from '../../components/common/Header'
 import { SideBar } from '../../components/common/SideBar'
+import Description from '../../components/common/Description'
+import {
+  MessageBubble,
+  sampleMessages,
+} from '../../components/common/MessageBubble'
 
 function Messages(): React.JSX.Element {
   return (
     <>
-      <div className="min-h-30">
-        <Header title="메시지" buttons={{ add: { show: true } }} />
-        <div>Messages Page</div>
-      </div>
-      <div className="flex">
-        <SideBar isAuthenticated={true} activeItem="/messages" />
+      <div className="flex min-h-screen">
+        <div className="h-full top-0 sticky">
+          <SideBar isAuthenticated={true} activeItem="/messages" />
+        </div>
+        <div className="mx-auto border-x border-background-border border-r border-l">
+          <Header title="메시지" buttons={{ add: { show: true } }} />
+          <div className="w-auto">
+            {/* 메세지가 없을때 */}
+            {sampleMessages.length === 0 ? (
+              <Description
+                iconType="post"
+                title="아직 포스트가 없습니다"
+                description="첫 번째 포스트를 작성해보세요"
+              />
+            ) : (
+              <>
+                {/* 메세지가 있을때 */}
+                {sampleMessages.map((msg) => (
+                  <MessageBubble
+                    type={msg.type}
+                    text={msg.text}
+                    userName={msg.userName} // 없으면 기본값 사용
+                  />
+                ))}
+              </>
+            )}
+          </div>
+        </div>
       </div>
     </>
   )
