7fa2e18ca93a5a77d4cd2f58591820b9a33c9b61
jiunlee19 <tearofglass@naver.com>
2025-09-18 11:33:28 +0900

    refactor(BaseButton, SidebarButton): Btns -> BaseButton, SidebarButton 분리

--- src/components/common/SidebarButton.tsx
+++ src/components/common/SidebarButton.tsx
@@ -2,7 +2,11 @@ interface SidebarButtonProps {
   /** 버튼에 표시될 텍스트 */
   content: string
   /** 버튼 아이콘 이미지 URL */
+<<<<<<< HEAD
   icon?: string
+=======
+  icon: string
+>>>>>>> dev
   /** 버튼에 대한 설명 */
   ariaLabel: string
   /** 버튼 활성화 상태 */
@@ -26,7 +30,11 @@ function SidebarButton({
   isActive = false,
   ariaLabel,
   onclick,
+<<<<<<< HEAD
 }: SidebarButtonProps) {
+=======
+}) {
+>>>>>>> dev
   return (
     <button
       aria-label={ariaLabel}
