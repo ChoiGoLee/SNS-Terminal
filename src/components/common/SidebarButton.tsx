dfaf40458d49b3b463079f95356507091b2d2617
이지언 <tearofglass@naver.com>
2025-09-18 13:12:51 +0900

    Update SidebarButton.tsx
    SidebarButton 충돌 해결

--- src/components/common/SidebarButton.tsx
+++ src/components/common/SidebarButton.tsx
@@ -2,11 +2,7 @@ interface SidebarButtonProps {
   /** 버튼에 표시될 텍스트 */
   content: string
   /** 버튼 아이콘 이미지 URL */
-<<<<<<< HEAD
   icon?: string
-=======
-  icon: string
->>>>>>> dev
   /** 버튼에 대한 설명 */
   ariaLabel: string
   /** 버튼 활성화 상태 */
@@ -30,11 +26,7 @@ function SidebarButton({
   isActive = false,
   ariaLabel,
   onclick,
-<<<<<<< HEAD
 }: SidebarButtonProps) {
-=======
-}) {
->>>>>>> dev
   return (
     <button
       aria-label={ariaLabel}
