fce3e0bfa25fab51ba9cef16dd95e1491f361b89
chlwlsgh777 <75794865+chlwlsgh777@users.noreply.github.com>
2025-09-19 17:32:06 +0900

    Revert "Feature/텍스트 input 컴포넌트"

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,17 +1,8 @@
 import React from 'react'
 import { Header } from '../../components/common/Header'
 import { SideBar } from '../../components/common/SideBar'
-import TextInput from '../../components/common/TextInput'
-import { useState } from 'react'
 
 function Home(): React.JSX.Element {
-  const [inputValue, setInputValue] = useState('')
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setInputValue(e.target.value)
-    console.log(e.target.value)
-  }
-
   return (
     <div className="flex min-h-screen">
       <div className="h-full">
@@ -19,17 +10,6 @@ function Home(): React.JSX.Element {
       </div>
       <div className="mx-auto border-x border-background-border border-r border-l">
         <Header title="홈" />
-        <TextInput
-          onchange={handleChange}
-          value={inputValue}
-          placeholder="기술 스택 검색"
-          size="md"
-          border={'fullRound'}
-          hasIcon={true}
-          id="password"
-          label="라벨"
-          type="text"
-        ></TextInput>
       </div>
     </div>
   )
