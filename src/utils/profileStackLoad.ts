76ce1b9640f919d57fa54f636afb3ef95b5ca135
woori ko <mynameiswooriko@gmail.com>
2025-09-24 02:42:16 +0900

    feat(profile-stack): 기술스택 데이터 추가 및 프로필 기술스택 불러오기 모듈 함수 추가(utils/profileStack.ts,utils/profileStackLoad.ts)

---
+++ src/utils/profileStackLoad.ts
@@ -0,0 +1,25 @@
+/**
+ * 프로필 기술스택 불러오기
+ *
+ * @param {string} introData - 유저정보의 자기소개(intro)
+ * @returns {string} '$%' 문자열로 분리된 '자기소개 텍스트'와 '기술스택'
+ *
+ */
+
+export const LoadIntroData = (introData: string) => {
+  let finalStack: string[]
+  let finalIntroduce: string
+
+  if (introData.includes('%$') === true) {
+    // '%$'문자열로 자기소개를 배열로 나눔
+    const splitIntroData = introData.split('%$')
+    // '자기소개 텍스트','기술스택'으로 나뉘어짐
+    finalIntroduce = splitIntroData[0]
+    finalStack = splitIntroData[1].split(',')
+  } else {
+    finalIntroduce = introData
+    finalStack = []
+  }
+
+  return { finalIntroduce, finalStack }
+}
