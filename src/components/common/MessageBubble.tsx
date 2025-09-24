1d63f670076ba14fd0eae3e7b9be7988efbb2549
jiunlee19 <tearofglass@naver.com>
2025-09-24 15:28:31 +0900

    refactor(message-bubble): 모든 채팅방 width 일치되도록 수정

--- src/components/common/MessageBubble.tsx
+++ src/components/common/MessageBubble.tsx
@@ -67,8 +67,8 @@ export const MessageBubble: React.FC<MessageBubbleProps> = ({
 
   if (type === 'me') {
     return (
-      <div className="flex flex-col items-end pl-4 pt-4 pr-4 pb-2 gap-2">
-        <div className="bg-primary rounded-2xl w-auto max-w-64 py-3 text-black">
+      <div className="flex flex-col items-end w-96 pl-4 pt-4 pr-4 pb-2 gap-2">
+        <div className="bg-primary rounded-2xl w-full max-w-64 py-3 text-black">
           <p className="px-4">{displayText}</p>
           {isLong && (
             <button
