b2c6063e918bad31f3842b90f2dc69031dad8cbe
jiunlee19 <tearofglass@naver.com>
2025-10-06 09:37:57 +0900

    Add ad integration to PageLayout component

--- src/components/layout/pageLayout.tsx
+++ src/components/layout/pageLayout.tsx
@@ -1,6 +1,19 @@
+import { useEffect, useRef } from 'react'
 import { useLocation, Outlet } from 'react-router-dom'
 
 import { SideBar } from '../common/SideBar'
+
+interface Adfit {
+  display: (unit: string) => void
+  destroy: (unit: string) => void
+  refresh: (unit: string) => void
+}
+
+declare global {
+  interface Window {
+    adfit?: Adfit
+  }
+}
 
 function PageLayout(): React.JSX.Element {
   const location = useLocation()
@@ -8,6 +21,39 @@ function PageLayout(): React.JSX.Element {
   const basePath = location.pathname.split('/')[1]
     ? `/${location.pathname.split('/')[1]}`
     : '/'
+
+  const adRef = useRef<HTMLDivElement>(null)
+  const adUnit = 'DAN-gcnELnsihVdOaDpF'
+
+  useEffect(() => {
+    // ins 요소 생성
+    const ins = document.createElement('ins')
+    ins.className = 'kakao_ad_area'
+    ins.setAttribute('data-ad-unit', adUnit)
+    ins.setAttribute('data-ad-width', '160')
+    ins.setAttribute('data-ad-height', '600')
+    ins.style.display = 'none'
+
+    // 스크립트 생성
+    const script = document.createElement('script')
+    script.setAttribute('src', '//t1.daumcdn.net/kas/static/ba.min.js')
+    script.setAttribute('type', 'text/javascript')
+    script.async = true
+
+    // DOM에 추가
+    if (adRef.current) {
+      adRef.current.appendChild(ins)
+      adRef.current.appendChild(script)
+    }
+
+    // 클린업
+    return () => {
+      const globalAdfit = window?.adfit
+      if (globalAdfit) {
+        globalAdfit.destroy(adUnit)
+      }
+    }
+  }, [])
 
   return (
     <div className="flex h-screen">
@@ -19,6 +65,11 @@ function PageLayout(): React.JSX.Element {
           <Outlet />
         </div>
       </div>
+      {/* 광고 영역 */}
+      <div
+        ref={adRef}
+        className="sticky top-0 h-screen flex items-center justify-center"
+      />
     </div>
   )
 }
